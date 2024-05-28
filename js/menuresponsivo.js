76bb4a116ee72104666ed2e7bfbc74819f84ee08
Denilson Abdon <102876445+AbdonOliver@users.noreply.github.com>
2024-05-28 16:20:12 -0300

    coomit upload

---
+++ js/menuresponsivo.js
@@ -0,0 +1,12 @@
+document.querySelector(".hamburguer").addEventListener("click", ()=>
+ document.querySelector(".menu-hamburguer").classList.toggle("show-menu")
+);
+document.querySelector(".hamburguer").addEventListener("click", ()=>
+ document.querySelector(".sidebar").classList.toggle("rigth-menu")
+);
+document.querySelector(".hamburguer").addEventListener("click", ()=>
+ document.querySelector(".img-responsiva").classList.toggle("none")
+);
+document.querySelector(".hamburguer").addEventListener("click", ()=>
+ document.querySelector(".texto-banner").classList.toggle("none")
+);
